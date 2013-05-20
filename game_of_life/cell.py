ac56fd979e820f7dd6e05b6385b916fd3c085ac2
Maxim Küpper <maxim.kuepper@gmail.com>
2013-05-20 18:17:44 +0200

    changed method of detecting number alive neighbors from search to active notify

--- game_of_life/cell.py
+++ game_of_life/cell.py
@@ -4,27 +4,29 @@ class Cell(object):
     def __init__(self, init_state, pos_x, pos_y):
         super(Cell, self).__init__()
 
-        self.next_state = init_state
+        self.current_state = init_state
+        self.alive_neighbors = 3 if init_state is 1 else 0
         self.pos_x = pos_x
         self.pos_y = pos_y
 
-    def kill(self):
-        self.next_state = 0
-
-    def reborn(self):
-        self.next_state = 1
-
     def cycle(self):
-        self.current_state = self.next_state
+        changed = False
+
+        if self.isAlive():
+            if self.alive_neighbors is not 2 and self.alive_neighbors is not 3:
+                self.current_state = 0
+                changed = True
+        else:
+            if self.alive_neighbors is 3:
+                self.current_state = 1
+                changed = True
+
+        self.alive_neighbors = 0
+
+        return changed
+
+    def addNeighbor(self):
+        self.alive_neighbors += 1
 
     def isAlive(self):
         return self.current_state is 1
-
-    def isDead(self):
-        return self.current_state is not 1
-
-    def getPosX(self):
-        return self.pos_x
-
-    def getPosY(self):
-        return self.pos_y
--- game_of_life/world.py
+++ game_of_life/world.py
@@ -10,11 +10,11 @@ class World(object):
     game_field = [[]]
     alive_marker = '#'
     dead_marker = ' '
-    width = 40
-    height = 20
+    width = 75
+    height = 50
     fill_percentage = 25
-    borders = True
-    cycle_delay = 100
+    borders = False
+    cycle_delay = 50
 
     def __init__(self):
         super(World, self).__init__()
@@ -24,81 +24,68 @@ class World(object):
 
     def run(self):
         cycles = 0
+        changed = False
 
         print "Starting a new Game! Width: " + str(self.width)\
             + " | Height: " + str(self.height)\
             + " | Initial Percantage: " + str(self.fill_percentage) + "%"
 
-        while (cycles is 0) or self.cycle():
+        while (cycles is 0) or changed:
             os.system('clear')
-            print "Cycle #" + str(cycles)
+            print "Cycle #" + str(cycles + 1)
             # only working on unix based OS
-            self.showWorld()
+            self.cycle()
+            changed = self.showWorld()
             cycles += 1
 
             time.sleep(self.cycle_delay / 1000)
 
         raw_input('Game endet...')
 
+
     def populateWorld(self):
         cellCount = self.width * self.height
         aliveCells = cellCount * self.fill_percentage / 100
         aliveChance = aliveCells / cellCount
 
-        self.game_field = [[[] for column in range(self.width)] for row in range(self.height)]
-
-        for row in range(self.height):
-            for cell in range(self.width):
-                state = 1 if (random() <= aliveChance) else 0
-                self.game_field[row][cell] = Cell(state, row, cell)
+        self.game_field = [
+            [
+                Cell(1, row, column) if (random() <= aliveChance) else Cell(0, row, column)
+                for column in range(self.width)]
+            for row in range(self.height)]
 
     def showWorld(self):
-        for row in self.game_field:
-            print '|',
-            for cell in row:
-                cell.cycle()
-                print (self.alive_marker if cell.isAlive() else self.dead_marker),
-            print '|'
-
-    def cycle(self):
         changed = False
 
         for row in self.game_field:
+            print '|',
             for cell in row:
-                changed = self.checkCell(cell) or changed
+                changed = cell.cycle() or changed
+                print (self.alive_marker if cell.isAlive() else self.dead_marker),
+            print '|'
 
         return changed
 
+    def cycle(self):
+        for row in self.game_field:
+            for cell in row:
+                self.checkCell(cell)
+
     def checkCell(self, cell):
-        alive_neighbors = self.getAliveNeighbors(cell)
-
-        if cell.isAlive() and (alive_neighbors is not 2 and alive_neighbors is not 3):
-            cell.kill()
-            return 1
-        elif cell.isDead() and alive_neighbors is 3:
-            cell.reborn()
-            return 1
-
-        return 0
-
-    def getAliveNeighbors(self, cell):
-        alive_neighbors = 0
-
-        for row in (range(cell.getPosX()-1, cell.getPosX()+2)):
-            if self.borders is True:
-                if row < 0 or row >= self.height:
-                    continue
-            else:
-                row = row % self.height
-
-            for column in (range(cell.getPosY()-1, cell.getPosY()+2)):
+        if cell.isAlive():
+            for row in range(cell.pos_x - 1, cell.pos_x + 2):
                 if self.borders is True:
-                    if column < 0 or column >= self.width:
+                    if row < 0 or row >= self.height:
                         continue
                 else:
-                    column = column % self.width
+                    row = row % self.height
 
-                if self.game_field[row][column].isAlive() and self.game_field[row][column] is not cell:
-                    alive_neighbors += 1
+                for column in range(cell.pos_y - 1, cell.pos_y + 2):
+                    if self.borders is True:
+                        if column < 0 or column >= self.width:
+                            continue
+                    else:
+                        column = column % self.width
 
-        return alive_neighbors
+                    if self.game_field[row][column] is not cell:
+                        self.game_field[row][column].addNeighbor()
